d44caf411eb90b43629d807a2f12cb3381452116
eabayev20 <eabayev20@gmail.com>
2021-01-07 11:05:02 -0500

    get made

--- Node.java
+++ Node.java
@@ -4,4 +4,15 @@ public class Node {
     next = null;
     prev = null;
   }
+  private String data;
+  private Node next, prev;
+  public String getData() {
+    return data;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev() {
+    return prev;
+  }
 }

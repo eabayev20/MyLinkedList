4a47fbbe77ce60a6541cf929316696f596152288
eabayev20 <eabayev20@gmail.com>
2021-01-07 11:07:05 -0500

    set made

--- Node.java
+++ Node.java
@@ -15,4 +15,13 @@ public class Node {
   public Node getPrev() {
     return prev;
   }
+  public void setData(String data){
+   this.data = data;
+ }
+ public void setNext(Node next){
+   this.next = next;
+ }
+ public void setPrev(Node prev){
+   this.prev = prev;
+ }
 }
